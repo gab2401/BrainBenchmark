8c1fa4f12490185db0b43747311661b8d8b95049
Gabriele Massara <87269867+gab2401@users.noreply.github.com>
2024-04-18 12:48:13 +0200

    Adding the frontend folder
    this folder doesn't have the node_modules folder because was too heavy for github, u can add it by opening the frontend folder on the webstorm software

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {FunctionComponent} from "react";
+
+const App: FunctionComponent = () => {
+  return (
+    <>
+      <h1>Questo e' un titolo</h1>
+      <ul>
+        <li>Primo</li>
+        <li>Secondo</li>
+        <li>Terzo</li>
+      </ul>
+    </>
+  )
+}
+
+export default App;
